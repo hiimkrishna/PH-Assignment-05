8917c9deeb642e1f9c5d55b6252299ad774d2558
KrishnaSinhaAnan <ksinhaanan1@gmail.com>
2025-08-29 20:57:42 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+
+    const heartButton = document.getElementById('heart');
+    const coinButton = document.getElementById('coin');
+    const copyButton = document.getElementById('copy');
+    const cardContainer = document.getElementById('cardContainer');
+    const historyContainer = document.getElementById('historyContainer');
+    const favourite = document.getElementById('favourite');
+
+
+
+  });
